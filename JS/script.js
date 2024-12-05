ed558f34fd1ceaccda183339cf30529f3cdc5ab4
Lia <examle@gmail.com>
2024-12-05 11:18:48 +0500

    1

--- JS/script.js
+++ JS/script.js
@@ -18,4 +18,31 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+
+        
+
+        } 
+
+    if (personalMovieDB.count < 10) {
+        console.log("Мало");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        console.log("Не плохо");
+    } else if(personalMovieDB.count >= 30){
+        console.log("Супер!")
+    } else {
+        console.log("Произошла ошибка")
+    }
+
+
 console.log(personalMovieDB);
+
